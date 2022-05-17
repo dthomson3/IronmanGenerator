d3d4bdc91abb011b0db8383628d20ceb847fa358
Dylan Thomson <dylan.levi.thomson@gmail.com>
2022-05-17 22:47:44 +1000

    cleaned up blazor app.

---
+++ IronmanGeneratorBlazorUI/RegisterServices.cs
@@ -0,0 +1,11 @@
+﻿namespace IronmanGeneratorBlazorUI;
+
+public static class RegisterServices
+{
+    public static void ConfirgureServies(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+
+    }
+}
